c1195f5f55983d6d6e05a08be9e2d56879233b15
Karina <usmkarin@gmail.com>
2023-07-20 11:43:12 +0700

    done

---
+++ model.py
@@ -0,0 +1,51 @@
+
+#осуществляем ф-ию открыть наши контакты:
+phone_book = {}
+path = 'phones.txt'
+
+def open_file():
+    with open(path, 'r', encoding='UTF-8') as file:
+        contacts = file.readlines()
+    for contact in contacts:
+        uid, name, phone, comment = contact.strip().split(';')        #стрип - убирает ненужные символы
+        phone_book[int(uid)] = [name, phone, comment]
+#открыли файл на чтение и сделали упорядоченный словарь
+
+
+#пишем ф-ию присвоения ID к новому контакту:
+def next_id():
+    return max(phone_book) + 1
+
+#ф-ия добавить контакт:
+def add_contact(new: list[str]):
+    phone_book[next_id()] = new
+
+#ф-ия сохранить изменения в файле:
+def save_file():
+    with open(path, 'w', encoding='UTF-8') as file:
+        contacts = []
+        for uid, contact in phone_book.items():
+            contacts.append(';'.join([str(uid), *contact]))
+        contacts = '\n'.join(contacts)
+        file.write(contacts)
+
+#ф-ия поиска по введенной с клавиатуры строке:
+def search(word: str) -> dict[int, list[str]]:
+    result = {}
+    for uid, contact in phone_book.items():
+        if word.lower() in ''.join(contact).lower():
+            result[uid] = contact
+    return result
+
+#ф-ия замены:
+def change(uid: int, new: list[str]) -> str:
+    contact = phone_book.get(uid)
+    for i in range(3):
+        if new[i] != '':
+            contact[i] = new[i]
+    phone_book[uid] = contact
+    return contact[0]
+
+#ф-ия удаления:
+def delete (uid: int) -> str:
+    return phone_book.pop(uid)[0]
